471d656a3f2738afd81e8c750698acdeec49b99b
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-06-19 20:23:00 +0300

    added styling to popups

--- src/list.tsx
+++ src/list.tsx
@@ -1,90 +1,51 @@
-import * as React from 'react';
 import { Link } from "@tanstack/react-router";
-import { PopupPlacement, Unstable_Popup as BasePopup } from '@mui/base/Unstable_Popup';
+import Home from "./List elements/Home";
+import MyAccounts from "./List elements/MyAccounts";
+import AddAccount from "./List elements/AddAccount";
+import Inbox from "./List elements/Inbox";
+import Following from "./List elements/Following";
+import Settings from "./List elements/Settings";
+
 
 
 
 
 
 function List(){
-
-  const [anchor, setAnchor] = React.useState<null | HTMLElement>(null);
-
-  const placement: PopupPlacement = 'right';
  
-
-  const open = Boolean(anchor);
-  const id = open ? 'simple-popper' : undefined;
 
     return(
 
       <ul className="grid grid-rows-[1fr_1fr_1fr_1fr_1fr_5fr] min-w-12 bg-stone-900 border-r-2 border-TradeSharegray">
       <li className="border-l-2 border-TradeShareblue px-3 min-h-16"> 
       <Link to="/">
-      <a aria-describedby={id} type="button" onMouseOver={(e)=>{setAnchor(e.currentTarget)}} onMouseOut={(e)=>{setAnchor(null)}} className="flex items-center text-gray-900 rounded-lg dark:text-white  group ">
-         <svg className="w-5 h-10 text-gray-500 transition duration-75 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-            <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-            <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-         </svg>
-      </a>
-      <BasePopup id={id} open={open} anchor={anchor} placement={placement}>
-        <div>The content of the Popup.</div>
-      </BasePopup>
-      </Link>
-
-
-
-      
+     <Home />
+      </Link>      
       </li>
 
       <li className="border-l-2 border-TradeShareblue px-3 min-h-16">
       <Link to= "/MyAccounts">
-      <a href="#" className="flex items-center text-gray-900 rounded-lg dark:text-white group">
-
-        
-         <svg className="flex-shrink-0 w-5 h-10 text-gray-500 transition duration-75 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-            <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
-         </svg>
-      </a>
+      <MyAccounts />
       </Link>
       </li>
       <li className="border-l-2 border-TradeShareblue px-3 min-h-16">
       <Link to="/AddAccount">
-      <a href="#" className="flex items-center text-gray-900 rounded-lg dark:text-white group">
-      <svg className="w-5 h-10 text-gray-500 transition duration-75 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-      </svg>
-   
-       
-      </a>
+      <AddAccount />
       </Link>
       </li>
       <li className="border-l-2 border-TradeShareblue px-3 min-h-16"> 
       <Link to="/Inbox">
-      <a href="#" className="flex items-center text-gray-900 rounded-lg dark:text-white group">
-         <svg className="flex-shrink-0 w-5 h-10 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-         </svg>
-      
-      </a>
+      <Inbox />
       </Link>
       </li>
       <li className="border-l-2 border-TradeShareblue px-3 min-h-16">
       <Link to="/Following">
-      <a href="#" className="flex items-center text-gray-900 rounded-lg dark:text-white group">
-         <svg className="flex-shrink-0 w-5 h-10 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-         </svg>
-      </a>
+      <Following />
       </Link>
       </li>
       <div className="bg-purple-700 px-3 min-h-16 bg-stone-900 ">
         <div className="mt-4 pt-64 ">
-      <a href="#" className="flex items-center text-gray-900 rounded-lg dark:text-white group">
-      <svg className= "flex-shrink-0 w-5 h-10 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">
-<path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/>
-</svg>
-      </a> 
+<Settings />
 </div>
       </div>
     </ul>
