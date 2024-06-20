fc913c981671485f04da531470fb8f8863eec9fc
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-06-20 12:53:16 +0300

    added Link Account MOdule

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import List from "./list";
 import { Link ,Outlet } from "@tanstack/react-router";
 import Statusbar from "./statusbar";
-import Module from "./Module";
 
 function App() {
   return (
-    <div className="grid grid-rows-[50fr_1fr] h-screen">
+    <div className="grid grid-rows-[35fr_1fr] h-screen">
       <div className="h-full bg-yellow-500 ">
         <div className="h-full grid grid-cols-[1fr_34fr]">
           <List />
-          <Module />
+          <Outlet />
         </div>
       </div>
-      <div className=" bg-blue-500"> 01 </div>
+      <div className=" bg-blue-500">
+      <div className="h-full grid grid-cols-[1fr_34fr] border-t-2 border-TradeSharegray">
+          <div className="bg-TradeShareblue min-w-16 px-2">
+           Status:
+          </div>
+          <div className="bg-black px-4">
+          Up 1%
+          </div>
+        </div>
+         </div>
     </div>
   );
 }
